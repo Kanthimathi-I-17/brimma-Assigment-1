5e5bf70af48fecec0427b04284b020e84f896845
Kanthimathi-I-17 <109584622+Kanthimathi-I-17@users.noreply.github.com>
2022-08-01 20:35:26 +0530

    Update 10,001stPrimeNumber.java

--- 10,001stPrimeNumber.java
+++ 10,001stPrimeNumber.java
@@ -1,3 +1,8 @@
+/*7. By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th
+prime is 13.
+What is the 10,001st prime number?
+Ans: 104743*/
+
 public class Main {
     public static void main(String args []){
         long numberOfPrimes = 0;
